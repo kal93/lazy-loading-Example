abcc27950688cf41d41fa11ecfb6fbeda631700e
kal93 <maaz2dt@gmail.com>
2018-03-23 15:40:39 +0530

    Removed typos.
    None.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 /** Configure the routes for the feature modules in this file
  * Feature modules here are the customers.module & orders.module
- * Reference: https://angular.io/guide/lazy-loading-ngmodules
  */
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,13 +7,14 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   // load customers module
   {
+   // lazy loading syntax i.e loadChildren followed by string path,# & module's class name
     path: 'customers',
     loadChildren: 'app/customers/customers.module#CustomersModule'
   },
   // loads orders modules
   {
     path: 'orders',
-    // lazy loading sytax i.e loadChildren followed by string path,# & module's class name
+    // lazy loading syntax i.e loadChildren followed by string path,# & module's class name
     loadChildren: 'app/orders/orders.module#OrdersModule'
   },
   // default route
